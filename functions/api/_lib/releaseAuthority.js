5f3f2674d58f9e2501903b8bdbe7807ee93278e4
John Rosevear <jfrosevear@gmail.com>
2026-08-28 13:00:15 -0400

    Open Release 448 current authority

--- functions/api/_lib/releaseAuthority.js
+++ functions/api/_lib/releaseAuthority.js
@@ -1,4 +1,4 @@
 // Devil n Dove single current-release authority.
-export const CURRENT_RELEASE = 447;
-export const RELEASE_LABEL = 'Platform Convergence';
+export const CURRENT_RELEASE = 448;
+export const RELEASE_LABEL = 'Platform Expansion';
 export const RELEASE_HEADER = 'X-DND-Release';
